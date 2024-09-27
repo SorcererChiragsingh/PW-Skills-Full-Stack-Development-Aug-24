8c0eb5cc53b16a298207297d9f14f082c1fec248
Chirag singh <chiragsingh8926@gmail.com>
2024-09-27 22:43:48 +0530

    Did some pratice

--- Module 18- Intro To JavaScript/index.js
+++ Module 18- Intro To JavaScript/index.js
@@ -5,3 +5,11 @@ console.log (1+"1");
 console.log (1-"1");
 
 console.log (3+9);
+
+// "comments in js" "for single line comments only" [Use '//']
+
+/* 
+    "This is for multiple line comments" [use "/*"]
+
+*/
+
