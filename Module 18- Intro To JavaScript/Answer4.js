067c04ca80ff8f08bdf931215361f8dbc0830b75
Chirag singh <chiragsingh8926@gmail.com>
2024-09-30 19:47:06 +0530

    Perform Q4 from Assignment

---
+++ Module 18- Intro To JavaScript/Answer4.js
@@ -0,0 +1,13 @@
+function generateNumbersBetween(num1, num2) 
+{
+    for (let i = num1 + 1; i <= num2; i++) 
+        {
+            console.log(i);
+        }
+  }
+  
+  // Example usage:
+  const num1 = 10;
+  const num2 = 25;
+  generateNumbersBetween(num1, num2);
+  
