17adca4113256f4fb528a9af77fc02700c978e1f
Chirag singh <chiragsingh8926@gmail.com>
2024-09-30 19:58:08 +0530

    Perform Q5 from Assignment

---
+++ Module 18- Intro To JavaScript/Answer5.js
@@ -0,0 +1,18 @@
+// Ascending Order (1 to 25):
+let i = 1;
+while (i <= 25) 
+    {
+        process.stdout.write(i + " ");
+        i++;
+    }
+    
+// Move to the next line after ascending order is printed
+console.log("");
+
+// Descending Order (25 to 1):
+let j = 25;
+while (j >= 1) 
+    {
+        process.stdout.write(j + " ");
+        j--;
+    }
