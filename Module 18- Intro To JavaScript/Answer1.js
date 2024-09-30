8a068a2e0ee18356102a8c0008dfa2772c55a01c
Chirag singh <chiragsingh8926@gmail.com>
2024-09-30 18:40:45 +0530

    Perform Q1 from Assignment

---
+++ Module 18- Intro To JavaScript/Answer1.js
@@ -0,0 +1,51 @@
+// if statement
+let age = 20;
+    if (age >= 18) 
+    {
+        console.log("You are eligible to vote.");
+    }
+
+// if...else statement
+let isRainy = false;
+    if (isRainy) 
+    {
+      console.log("Take an umbrella.");
+    } 
+    else 
+    {
+      console.log("No need for an umbrella.");
+    }
+
+//  if...else if...else statement   
+let score = 85;
+    if (score >= 90) 
+        {
+            console.log("Grade: A");
+        } 
+        else if (score >= 80) 
+            {
+                console.log("Grade: B");
+            } 
+            else if (score >= 70) 
+                {
+                    console.log("Grade: C");
+                } 
+                else 
+                {
+                    console.log("Grade: F");
+                }
+
+// switch statement
+let day = "Tuesday";
+switch(day) 
+{
+  case "Monday":
+    console.log("It's Monday!");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday!");
+    break;
+  default:
+    console.log("It's another day.");
+}
+
